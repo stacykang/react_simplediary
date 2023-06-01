7594c5210daff4f7892a96201c2e8e5717751356
stacykang <mangoe@naver.com>
2023-06-01 15:48:19 +0900

    :memo: Add dummy list data & Pass as prop

--- src/App.js
+++ src/App.js
@@ -2,11 +2,36 @@ import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
+//예시용 임시 배열 생성, diaryList에 prop으로 데이터 전달
+const dummyList = [
+    {
+        id: 1,
+        author: "강현주",
+        content: "오늘은 리액트를 공부했다",
+        emotion: 2,
+        create_date: new Date().getTime(),
+    },
+    {
+        id: 2,
+        author: "강현주",
+        content: "마라탕을 먹었다.",
+        emotion: 3,
+        create_date: new Date().getTime(),
+    },
+    {
+        id: 3,
+        author: "강현주",
+        content: "구름이랑 산책을 갔다",
+        emotion: 1,
+        create_date: new Date().getTime(),
+    },
+];
+
 function App() {
     return (
         <div className="App">
             <DiaryEditor />
-            <DiaryList />
+            <DiaryList diaryList={dummyList} />
         </div>
     );
 }
--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,7 +1,9 @@
-const DiaryList = () => {
+const DiaryList = ({ diaryList }) => {
+    console.log(diaryList);
     return (
         <div className="DiaryList">
             <h2>일기 리스트</h2>
+            <p>{diaryList.length}개의 일기가 있습니다.</p>
         </div>
     );
 };

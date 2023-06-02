639b5b484de65ecd41e86aeb155f37ab46029305
stacykang <mangoe@naver.com>
2023-06-03 02:01:50 +0900

    :art: Render DiaryList using Array.map() method

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,9 +1,22 @@
+const arrayA = [1, 2, 3];
+
 const DiaryList = ({ diaryList }) => {
+    console.log(arrayA);
     console.log(diaryList);
     return (
         <div className="DiaryList">
             <h2>일기 리스트</h2>
             <p>{diaryList.length}개의 일기가 있습니다.</p>
+            <div>
+                {diaryList.map((item) => (
+                    <div>
+                        <div>작성자 : {item.author}</div>
+                        <div>일기 : {item.content}</div>
+                        <div>감정 : {item.emotion}</div>
+                        <div>작성 시간(ms) : {item.create_date}</div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
